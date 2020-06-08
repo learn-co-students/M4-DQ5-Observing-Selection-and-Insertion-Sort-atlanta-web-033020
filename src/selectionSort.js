fb74798ab442744bd3379cef5333bf90d9777d34
github username <github email address>
2020-06-08 17:10:02 -0400

    Done.

--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,31 @@
+function selectionSort2(arr) {
+  let unsortedIndex = 0;
+  while (unsortedIndex < arr.length) {
+    let smallestIndex = unsortedIndex;
+    for (let i = unsortedIndex; i < arr.length; i++) {
+      if (arr[i] < arr[smallestIndex]) {
+        smallestIndex = i;
+      }
+    }
+    swap(arr, smallestIndex, unsortedIndex)
+    unsortedIndex++;
+  }
+}
+
 function selectionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for (let i = 0; i < arr.length; i++) {
+    let smallestIndex = i;
+    for (let j = i; j < arr.length; j++) {
+      if (arr[j] < arr[smallestIndex]) {
+        smallestIndex = j;
+      }
+    }
+    swap(arr, smallestIndex, i);
+  }
+}
+
+function swap(arr, a, b) {
+  let temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
 }
